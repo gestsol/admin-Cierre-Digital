fa6f01b67bc0fc3dc9768ccd7d1da445176f0ecd
Dorian <dorian.celu@gmail.com>
2023-02-07 16:57:06 -0300

    cambio de pasw

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 function conectar(){
-    $host="localhost";
-    $user="root";
-    $pass="";
+    $host="ls-233cbb9833b65f1d81c7123bc6f953c2597da307.cwdutvqwihot.us-east-1.rds.amazonaws.com";
+    $user="dbmasteruser";
+    $pass="J&ye#Ve3*f8dYR~XP~8V(uAj0%Xs|Pe2";
 
     $bd="keylock";
 
